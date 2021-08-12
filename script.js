a992b7100b315f78c66ad259dcd035b8bbc3b3d1
Le Hong Long <lehonglong2411@gmail.com>
2021-08-12 22:53:15 +0700

    Build 1.0.0

---
+++ script.js
@@ -0,0 +1,60 @@
+// menu show
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById ( toggleId ),
+    nav = document.getElementById (navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// Gan bien class
+const navLink = document.querySelectorAll('.nav_link')
+
+
+// Switch active status
+function linkAction(){
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // syntax error
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// scoll animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+// scroll home
+sr.reveal('.home_title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home_img', {delay: 400})
+sr.reveal('.home_social-icon', {interval: 200})
+
+// scroll about
+sr.reveal('.about_img', {})
+sr.reveal('.about_subtitle', {delay: 200})
+sr.reveal('.about_text', {delay: 400})
+
+// scroll skills
+sr.reveal('.skills_subtitle', {})
+sr.reveal('.skills_text', {delay: 200})
+sr.reveal('.skills_data', {interval: 200})
+sr.reveal('.skills_img', {delay: 400})
+
+// scroll work
+sr.reveal('.work_img', {interval: 200})
+
+// scroll contact
+sr.reveal('.contact_input', {interval: 200})
